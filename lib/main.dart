d07443c7fd2f629d6fb6efe2bfdacb02117103df
octobrekids <chnrd.k@gmail.com>
2021-06-02 21:44:41 +0700

    add ball1 image

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {},
+              child: Image.asset('images/ball1.png'),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
